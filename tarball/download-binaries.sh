08eb6261b82f27d10c2acae21a2b3812384a0ab2
sam bacha <sam@manifoldfinance.com>
2022-07-07 02:23:54 -0700

    chore: cleanup

--- tarball/download-binaries.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Downloading binaries"
-wget https://github.com/numtide/treefmt/releases/download/v0.2.6/treefmt-x86_64-unknown-linux-musl.tar.gz.sha256
-wget https://github.com/numtide/treefmt/releases/download/v0.2.6/treefmt-x86_64-unknown-linux-musl.tar.gz
